03e93dcb0b7fb6711e85b6eef447e415c8059daf
nonishi <g.nonono24@gmail.com>
2021-06-11 14:02:31 +0000

    [4-10]Update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '46ad2ad62a6b0ebc2866ceef16b3ffba84d6aa22b1c7eb8a584268ac07dcf570f5706d47775e4c9cd2b42ad438bb815f47d332ae624445988463e2f83f72d98d'
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
-  resources :books
-  resources :users
+  resources :books, except: [:new]
+  resources :users, only: [:index, :show, :edit, :update]
 end
