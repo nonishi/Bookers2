bdeb9fc5626b4abbb40100d5f4b69f048717ece9
nonishi <g.nonono24@gmail.com>
2021-07-09 09:30:23 +0000

    [Update]応用課題7c

---
+++ app/models/group.rb
@@ -0,0 +1,4 @@
+class Group < ApplicationRecord
+  belongs_to :group
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_03_042246) do
+ActiveRecord::Schema.define(version: 2021_07_09_091733) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -31,6 +31,22 @@ ActiveRecord::Schema.define(version: 2021_07_03_042246) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "group_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "introduction"
+    t.integer "image_id"
+    t.integer "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
