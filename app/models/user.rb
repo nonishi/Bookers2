5237b7a8870500210cd737f565d344f17bb5ecea
nonishi <g.nonono24@gmail.com>
2021-07-13 10:07:34 +0000

    [Update]応用課題7c

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,6 @@ class User < ApplicationRecord
   has_many :followings, through: :follower, source: :followed
   has_many :followers, through: :followed, source: :follower
   
-  
-  
   attachment :profile_image
 
   validates :name, uniqueness: true, length: { in: 2..20 }
