55f665056ee7395b1670569a712c562e709db2a6
nonishi <g.nonono24@gmail.com>
2021-09-04 00:12:00 +0000

    [Finish]応用課題9b

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,27 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: :homes_about
-  
+
   get '/search' => 'searches#search'
-  
+
   resources :books, except: [:new] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
+    get "search", to: "users#search"
   end
-  
+
   resources :groups do
     get "join" => "groups#join"
   end
-  
+
   resources :chats, only: [:show, :create]
-  
+
 end
